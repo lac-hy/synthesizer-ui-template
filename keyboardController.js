c77366c63231126b7935d7c2c29544ab154ae04f
lac-hy <lachy4work@gmail.com>
2025-08-27 13:42:09 +1000

    changes

--- keyboardController.js
+++ keyboardController.js
@@ -11,10 +11,51 @@ let allKeys = Array.from(document.getElementsByClassName("whiteKey")).concat(
 /* set default octace : we will update based on keys later on */
 let octave = 3;
 
+let mouseHeld = false;
+
+window.addEventListener("mousedown", () => {
+  mouseHeld = true;
+});
+
+window.addEventListener("mouseup", () => {
+  mouseHeld = false;
+});
+
 /* add an event listener to each key */
 allKeys.forEach((key) => {
   key.addEventListener("mousedown", (e) => {
     let note = e.target.dataset.note;
-    polySynth.triggerAttackRelease(note + octave, "8n");
+    polySynth.triggerAttack(note + octave);
+  });
+  key.addEventListener("mouseup", (e) => {
+   if (mouseHeld === false) {
+      return;
+    }
+    let note = e.target.dataset.note;
+    polySynth.triggerRelease(note + octave, "8n");
+  });
+  key.addEventListener("mouseenter", (e) => {
+    let note = e.target.dataset.note;
+    polySynth.triggerAttack(note + octave);
+  });
+  key.addEventListener("mouseleave", (e) => {
+    let note = e.target.dataset.note;
+    polySynth.triggerRelease(note + octave, "8n");
   });
 });
+
+/* event listener for keyboard (qwerty) */
+window.addEventListener("keydown", (e) => {
+ if(e.code === "KeyA"){
+  if(keyHeld === false){
+   polySynth.triggerAttack("c3");
+   keyHeld = true;
+  }
+ }
+});
+window.addEventListener("keyup", (e) => {
+  if (e.code === "KeyA"){
+    polySynth.triggerRelease("c3");
+    keyHeld = false;
+  }
+})
